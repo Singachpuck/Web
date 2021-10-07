94d07a8c308b1260835f4cdec53c056144920f6c
Singachpuck <singachpucktv@gmail.com>
2021-10-07 23:55:43 +0300

    Lab 2

---
+++ ЛР2/js/script.js
@@ -0,0 +1,64 @@
+window.onload = () => {
+    let firstBlock = document.querySelector('aside'),
+        secondBlock = document.querySelector('.header-section > .small-paragraph'),
+        thirdBlock = document.querySelector('.main-section > p'),
+        splitButton = document.querySelector('#split-button');
+
+    const a = 2, b = 4;
+
+    init();
+
+    swapText(firstBlock, secondBlock);
+
+    thirdBlock.innerHTML += ellipsisSquare(a, b).toFixed(3);
+
+    splitButton.addEventListener('click', () => {
+        const number = parseInt(document.querySelector('#split-number-input').value),
+            dividers = splitNumber(number);
+        
+        document.cookie = `splitNumber=${dividers.toString()};`;
+
+        console.log(document.cookie);
+
+        alert(dividers);
+    });
+
+    function init() {
+        const calculatorBlock = document.querySelector('.divider-calculator');
+
+        if (document.cookie !== '') {
+            calculatorBlock.setAttribute('style', 'display: none;');
+
+            if (confirm(`Cookie data:\n${document.cookie}\nDo you want to remove them?`)) {
+                document.cookie = '';
+                init();
+            } else {
+                alert('Cookies are used on this page!\nReload page to change that.');
+            }
+        } else {
+            calculatorBlock.removeAttribute('style');
+        }
+    }
+
+    function swapText(el1, el2) {
+        let tmp = el1.innerHTML;
+        el1.innerHTML = el2.innerHTML;
+        el2.innerHTML = tmp;
+    }
+
+    function ellipsisSquare(a, b) {
+        return Math.PI * a * b;
+    }
+
+    function splitNumber(number) {
+        const dividers = [];
+
+        for (let i = 2; i < number; i++) {
+            if (number % i === 0) {
+                dividers.push(i);
+            }
+        }
+
+        return dividers;
+    }
+}
